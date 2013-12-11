f730bbc0bfb1163ae956490a32a6dd9c11630321
alejandroloreto <cano_als_10@hotmail.com>
2013-12-11 00:07:58 -0600

    practica1

---
+++ practica1/practica1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace practica1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("--PROGRAMACION 3/PRACTICA 1--");
+			
+		
+			Console.ReadKey(true);
+		}
+	}
+}
